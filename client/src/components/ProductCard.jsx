3ffd14c727b8dbc4b55deb6214c70fdfed01ddd4
Ganesh Aroda C <gani40008@gmail.com>
2026-07-25 21:12:29 +0530

    updating

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,10 +2,14 @@ import "./ProductCard.css";
 
 function ProductCard(props) {
     return (
-        <div className="product-card">
+        <div 
+            className="product-card"
+            onClick={()=>alert("Open product Details")}
+        >
             <span>{props.product.icon}</span>
             <h3>{props.product.name}</h3>
             <p>₹{props.product.price}</p>
+            
         </div>
     );
 }
