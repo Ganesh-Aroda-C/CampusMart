71e224b2ee64fb32077b84e858cbad9d9976ac50
Ganesh Aroda C <gani40008@gmail.com>
2026-07-27 16:46:22 +0530

    updated productdetails

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -4,6 +4,9 @@ function ProductDetails() {
     const { id } = useParams();
     const productId = Number(id);
     const product = products.find(product => product.id === productId);
+    if (!product) {
+    return <h1>Product Not Found</h1>;
+}
     return (
         <>
             <h1>{product.name}</h1>
